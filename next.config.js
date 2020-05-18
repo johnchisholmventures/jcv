003ff4911cb0589dda064419fa0719a66ff332df
agstover <agstover@gmail.com>
2020-05-17 22:23:16 -0500

    removed withCSS

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-require('dotenv').config();
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+require('dotenv').config()
 
-module.exports = withCss({
+module.exports = {
   env: {
     TWITTER_API_TOKEN: process.env.TWITTER_API_TOKEN,
     // TWITTER_LABS_ENABLED: '1',
     // TWITTER_LABS_EXPANSIONS: 'attachments.poll_ids',
     // TWITTER_LOAD_WIDGETS: false,
   },
-})
+};
