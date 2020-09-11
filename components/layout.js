6bf5773a6f8d036b18a8fe998eead6fbbea1afcd
johnchisholmventures <andrew@johnchisholmventures.com>
2020-09-11 00:10:04 -0500

    eight round of revision

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children }) {
         <Alert>
           <div className='flex flex-row justify-center align-items-center'>
             <p className='pr-4'>
-              <span className='font-bold'>Are you an educator?</span>
+              <span className='font-bold'>Do you teach entrepreneurship?</span>
               <Link href='/educators'>
                 <a className='text-teal-400 hover:text-teal-500' href='/educators'> Click here </a>
               </Link>
