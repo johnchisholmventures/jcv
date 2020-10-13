4e719bcd2d42325c20d02d451cbde656046eb833
johnchisholmventures <andrew@johnchisholmventures.com>
2020-10-13 13:55:00 -0500

    added pdf functionality, changed default social picture, updated header logo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 require('dotenv').config()
 
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/cv',
+        destination: '/pdfs/chisholm_cv_november_2020.pdf',
+      },
+      {
+        source: '/regulation',
+        destination: '/pdfs/regulation.pdf',
+      }
+    ]
+  },
   env: {
     TWITTER_API_TOKEN: process.env.TWITTER_API_TOKEN,
     // TWITTER_LABS_ENABLED: '1',
